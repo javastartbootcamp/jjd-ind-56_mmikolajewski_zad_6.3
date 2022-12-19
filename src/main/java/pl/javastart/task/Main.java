1a7edaf81c9f8ef5aab873c492f0a541fcfb705c
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2022-12-19 13:01:01 +0000

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,7 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        ArrayUtils utils = new ArrayUtils();
+        int[][] array = utils.createArray();
     }
 }
